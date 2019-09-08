497c3448b34edad9db3a9a6c43233b54a2aa9a79
cmiley <cmiley@nevada.unr.edu>
2019-09-08 04:39:31 -0700

    OpenCV working state.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorOpMode.java
@@ -3,9 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 @TeleOp()
-public class ColorSensorOpMode extends OpMode {
+public class ColorSensorOpMode extends OpMode implements CameraBridgeViewBase.CvCameraViewListener2 {
+
     private Robot robot = new Robot();
+    private CameraBridgeViewBase mOpenCvCameraView;
 
     // Code to run ONCE when the driver hits INIT
     @Override
@@ -19,5 +29,20 @@ public class ColorSensorOpMode extends OpMode {
         telemetry.addData("getGreen", robot.getGreen());
         telemetry.addData("Red", robot.getRed());
         telemetry.addData("blue", robot.getBlue());
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        return;
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        return;
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
     }
 }
