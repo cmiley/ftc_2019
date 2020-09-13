1d47f04ed9b197d16675213bdf8e6c01bb9b5948
Bryson Lingenfelter <blingenfelter@nevada.unr.edu>
2020-09-12 23:02:44 -0700

    many debug messages

--- DogeCV/src/main/java/com/disnodeteam/dogecv/detectors/StartDetector.java
+++ DogeCV/src/main/java/com/disnodeteam/dogecv/detectors/StartDetector.java
@@ -34,6 +34,8 @@ public class StartDetector extends DogeCVDetector {
 
     @Override
     public Mat process(Mat input) {
+        Log.e("Debug", "hi, I'm a debug message!");
+        Log.e("Debug", "shape: " + input.width() + " " + input.height());
 
         // Copy the input mat to our working mats, then release it for memory
         input.copyTo(displayMat);
@@ -41,26 +43,31 @@ public class StartDetector extends DogeCVDetector {
         input.release();
 
         frameNumber++;
+        Log.e("Debug", "frame no: " + frameNumber);
         if(!color_detected && frameNumber > 0) {
             Mat hsl = new Mat();
             Imgproc.cvtColor(input, hsl, Imgproc.COLOR_BGR2HSV);
+            Log.e("Debug", "color converted");
 
             Mat mask_red = new Mat();
             Mat mask_blue = new Mat();
             Core.inRange(hsl, new Scalar(80,120,100), new Scalar(120,255,255), mask_blue);
             Core.inRange(hsl, new Scalar(0,120,100), new Scalar(45,255,255), mask_red);
             hsl.release();
+            Log.e("Debug", "thresholding done");
 
             Rect cropbox = new Rect(0,2500,4048,536);
             Mat cropped_red = new Mat(mask_red, cropbox);
             Mat cropped_blue = new Mat(mask_blue, cropbox);
             mask_red.release();
             mask_blue.release();
+            Log.e("Debug", "cropping done");
 
             double red_count = Core.sumElems(cropped_red).val[0]/255;
             double blue_count = Core.sumElems(cropped_blue).val[0]/255;
             cropped_red.release();
             cropped_blue.release();
+            Log.e("Debug", "counting done");
 
             if ((red_count > 0) || (blue_count > 0))
                 color_detected = true;
@@ -68,6 +75,7 @@ public class StartDetector extends DogeCVDetector {
                 start_color = 1;
             else
                 start_color = 2;
+            Log.e("Debug", "color selected");
         }
         return displayMat;
     }
