8f3184ee031b056c841b1480ba459b6ac17a6314
cmiley <cmiley@nevada.unr.edu>
2020-09-13 01:51:39 -0700

    Added Ring detector. It doesn't work.

---
+++ DogeCV/src/main/java/com/disnodeteam/dogecv/detectors/RingDetector.java
@@ -0,0 +1,79 @@
+package com.disnodeteam.dogecv.detectors;
+
+import android.util.Log;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class RingDetector extends DogeCVDetector {
+
+    // Defining Mats to be used.
+    private Mat displayMat = new Mat(); // Display debug info to the screen (this is what is returned)
+    private Mat workingMat = new Mat(); // Used for preprocessing and working with (blurring as an example)
+
+    public int ring_count;
+    public boolean color_detected;
+    private int frameNumber;
+
+    /**
+     * Simple constructor
+     */
+    public RingDetector() {
+        super();
+        detectorName = "Start Detector"; // Set the detector name
+        ring_count = 0;
+        color_detected = false;
+        frameNumber = 0;
+    }
+
+
+    @Override
+    public Mat process(Mat input) {
+//        Log.d("StartDetector", "hi, I'm a debug message!");
+//        Log.d("StartDetector", "shape: " + input.width() + " " + input.height());
+        // Copy the input mat to our working mats, then release it for memory
+        input.copyTo(displayMat);
+        input.copyTo(workingMat);
+        input.release();
+
+        Imgproc.rectangle(displayMat, new Point(400, 0), new Point(640, 480), new Scalar(0, 0, 255),5);
+
+        frameNumber++;
+//        Log.d("StartDetector", "Dump mat:" + workingMat.dump());
+//        Log.d("StartDetector", "frame no: " + frameNumber);
+        if(!color_detected && frameNumber > 0) {
+            Mat hsv = new Mat();
+            Imgproc.cvtColor(workingMat, hsv, Imgproc.COLOR_BGR2HSV);
+            Log.d("RingDetector", "color converted");
+
+            Mat mask = new Mat();
+            Core.inRange(hsv, new Scalar(5,160,80), new Scalar(45,240,240), mask);
+            hsv.release();
+            Log.d("RingDetector", "thresholding done");
+
+            Rect cropbox = new Rect(0,300,450,350);
+            Mat cropped = new Mat(mask, cropbox);
+            mask.release();
+
+            double orange_pixel_count = Core.sumElems(cropped).val[0]/255;
+            cropped.release();
+            Log.d("RingDetector", "orange pixels: " + orange_pixel_count);
+            int ring_count = 0;
+            if (orange_pixel_count > 1000)
+                ring_count = 1;
+            if (orange_pixel_count > 4000)
+                ring_count = 4;
+        }
+        return displayMat;
+    }
+
+    @Override
+    public void useDefaults() {
+
+    }
+
+}
